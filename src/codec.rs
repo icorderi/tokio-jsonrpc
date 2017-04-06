4f4480020c4a71a3ea95972d6620b640f6950c95
Ignacio Corderi <icorderi@msn.com>
2017-04-05 17:55:06 -0700

    feat: add netstring codec

--- src/codec.rs
+++ src/codec.rs
@@ -102,6 +102,236 @@ impl Decoder for Line {
     }
 }
 
+pub use codec::netstring::Netstring;
+
+// TODO: module documentation
+pub mod netstring {
+
+    use std::io::Cursor;
+
+    use bytes::Buf;
+
+    use super::*;
+
+    // The following empty netstring `0:,` is the smallest one
+    const MINIMUM_NETSTRING: usize = 3;
+
+    const NETSTRING_TAIL: &'static [u8] = &[b','];
+
+    /// Configure netstring delimited `Framed` values.
+    ///
+    /// `Builder` enables constructing configured netstring delimited framers. Note
+    /// that not all configuration settings apply to both encoding and decoding. See
+    /// the documentation for specific methods for more detail.
+    #[derive(Debug, Clone, Copy)]
+    pub struct Builder {
+        // Maximum frame length
+        max_frame_len: usize,
+
+        // Number of bytes in the header before the length field
+        length_field_offset: usize,
+    }
+
+
+    impl Builder {
+        /// Creates a new length delimited framer builder with default configuration
+        /// values.
+        pub fn new() -> Self {
+            Builder {
+                // Default max frame length of 32MB
+                max_frame_len: 32 * 1_024 * 1_024,
+
+                // Default to the header field being at the start of the header.
+                length_field_offset: 0,
+            }
+        }
+
+        /// Sets the max frame length
+        ///
+        /// This configuration option applies to both encoding and decoding. The
+        /// default value is 8MB.
+        ///
+        /// When decoding, the length field read from the byte stream is checked
+        /// against this setting **before** any adjustments are applied. When
+        /// encoding, the length of the submitted payload is checked against this
+        /// setting.
+        pub fn max_frame_length(&mut self, val: usize) -> &mut Self {
+            self.max_frame_len = val;
+            self
+        }
+
+        /// Sets the number of bytes in the header before the length field
+        ///
+        /// This configuration option only applies to decoding.
+        pub fn length_field_offset(&mut self, val: usize) -> &mut Self {
+            self.length_field_offset = val;
+            self
+        }
+
+        /// Create a configured length delimited `Netstring` codec
+        pub fn build(&self) -> Netstring {
+            Netstring {
+                builder: *self,
+                decoder_state: DecodeState::Head,
+            }
+        }
+    }
+
+    /// A codec working with JSONRPC 2.0 messages.
+    ///
+    /// This produces or encodes [Message](../message/enum.Message.hmtl). It separates the records
+    /// using netstring frames.
+    ///
+    /// Note that the produced items is a `Result`, to allow not terminating the stream on
+    /// protocol-level errors.
+    #[derive(Debug)]
+    pub struct Netstring {
+        builder: Builder,
+        decoder_state: DecodeState,
+    }
+
+    impl Netstring {
+        /// A constructor
+        pub fn new() -> Self {
+            Builder::new().build()
+        }
+
+        fn decode_head(&mut self, src: &mut BytesMut) -> IoResult<Option<usize>> {
+            if src.len() < self.builder.length_field_offset + MINIMUM_NETSTRING {
+                // Not enough data
+                return Ok(None);
+            }
+
+            let (n, i) = {
+                let mut src = Cursor::new(&mut *src);
+
+                // Skip the required bytes
+                src.advance(self.builder.length_field_offset);
+
+                // Find the next `:` delimiting the end of the length
+                if let Some(i) = src.bytes()
+                       .iter()
+                       .position(|b| *b == b':') {
+                    // Parse length
+                    let n: u64 = match String::from_utf8(src.bytes()[..i].to_vec()) {
+                        Ok(s) => {
+                            s.parse()
+                                .map_err(|_| {
+                                    Error::new(ErrorKind::InvalidData, "Could not parse length")
+                                })?
+                        },
+                        Err(err) => return Err(Error::new(ErrorKind::InvalidData, err)),
+                    };
+
+                    if n > self.builder.max_frame_len as u64 {
+                        return Err(Error::new(ErrorKind::InvalidData, "frame size too big"));
+                    }
+
+                    // The check above ensures there is no overflow
+                    (n as usize, i)
+                } else {
+                    return Ok(None);
+                }
+            };
+
+           // | length_field_offset | netstring |':'| payload
+            let num_skip = self.builder.length_field_offset + i + 1;
+            let _ = src.split_to(num_skip);
+
+            // Ensure that the buffer has enough space to read the incoming
+            // payload
+            // Note: there is a ',' after the payload
+            src.reserve(n + 1);
+
+            return Ok(Some(n));
+        }
+
+        fn decode_data(&self, n: usize, src: &mut BytesMut) -> IoResult<Option<BytesMut>> {
+            // At this point, the buffer has already had the required capacity
+            // reserved. All there is to do is read.
+            // Note: The `+1` is for the ',' after the payload
+            if src.len() < n + 1 {
+                return Ok(None);
+            }
+
+            // Get the content
+            let content = src.split_to(n);
+
+            // Remove the ',' at the end
+            let _ = src.split_to(1);
+
+            Ok(Some(content))
+        }
+    }
+
+    #[derive(Debug, Clone, Copy)]
+    enum DecodeState {
+        Head,
+        Data(usize),
+    }
+
+    impl Default for DecodeState {
+        fn default() -> Self {
+            DecodeState::Head
+        }
+    }
+
+    impl Encoder for Netstring {
+        type Item = Message;
+        type Error = Error;
+        fn encode(&mut self, msg: Message, buf: &mut BytesMut) -> IoResult<()> {
+            // netstring format = <ln>:<payload>,
+            let payload = to_vec(&msg).map_err(err_map)?;
+            let head = format!("{}:", payload.len());
+
+            let n = head.len() + payload.len() + 1;
+            if n > self.builder.max_frame_len {
+                return Err(Error::new(ErrorKind::InvalidInput, "frame size too big"));
+            }
+
+            buf.reserve(n);
+
+            buf.put_slice(head.as_bytes());
+            buf.put_slice(&payload);
+            buf.put(NETSTRING_TAIL);
+            Ok(())
+        }
+    }
+
+    impl Decoder for Netstring {
+        type Item = Parsed;
+        type Error = Error;
+        fn decode(&mut self, src: &mut BytesMut) -> IoResult<Option<Parsed>> {
+            let n = match self.decoder_state {
+                DecodeState::Head => {
+                    match self.decode_head(src)? {
+                        Some(n) => {
+                            self.decoder_state = DecodeState::Data(n);
+                            n
+                        },
+                        None => return Ok(None),
+                    }
+                },
+                DecodeState::Data(n) => n,
+            };
+
+            match self.decode_data(n, src)? {
+                Some(data) => {
+                    // Update the decode state
+                    self.decoder_state = DecodeState::Head;
+
+                    // Make sure the buffer has enough space to read the next head
+                    src.reserve(self.builder.length_field_offset + MINIMUM_NETSTRING);
+
+                    // Build `Parsed` from buf
+                    Ok(Some(from_slice(&data)))
+                },
+                None => Ok(None),
+            }
+        }
+    }
+}
+
 /// A codec working with JSONRPC 2.0 messages on top of badly encoded utf-8.
 ///
 /// This works like the [Line](struct.Line.html) codec. However, it can cope with the input not
@@ -234,5 +464,46 @@ mod tests {
         let result = dirty.decode(&mut buf).unwrap();
         assert_eq!(result,
                    Some(Ok(Message::notification("Hello �World".to_owned(), None))));
+    }
+
+    #[test]
+    fn encode_netstring() {
+        let input = Message::notification("notif".to_owned(), None);
+        let mut output = BytesMut::with_capacity(10);
+
+        // default codec
+        let mut codec = Netstring::new();
+        codec.encode(input.clone(), &mut output).unwrap();
+        let encoded = BytesMut::from(&br#"34:{"jsonrpc":"2.0","method":"notif"},"#[..]);
+        assert_eq!(encoded, output);
+        output.clear();
+
+        // Cap frame length
+        let mut codec = netstring::Builder::new().max_frame_length(10).build();
+        codec.encode(input.clone(), &mut output).unwrap_err();
+        output.clear();
+    }
+
+    #[test]
+    fn decode_netstring() {
+        let expected = Message::notification("notif".to_owned(), None);
+        let expected = Some(Ok(expected));
+
+        // default codec
+        let mut codec = Netstring::new();
+        let mut encoded = BytesMut::from(&br#"34:{"jsonrpc":"2.0","method":"notif"},"#[..]);
+        let decoded = codec.decode(&mut encoded).unwrap();
+        assert_eq!(expected, decoded);
+
+        // Cap frame length
+        let mut codec = netstring::Builder::new().max_frame_length(10).build();
+        let mut encoded = BytesMut::from(&br#"34:{"jsonrpc":"2.0","method":"notif"},"#[..]);
+        codec.decode(&mut encoded).unwrap_err();
+
+        // codec with head offset
+        let mut codec = netstring::Builder::new().length_field_offset(6).build();
+        let mut encoded = BytesMut::from(&br#"MAGIC:34:{"jsonrpc":"2.0","method":"notif"},"#[..]);
+        let decoded = codec.decode(&mut encoded).unwrap();
+        assert_eq!(expected, decoded);
     }
 }
